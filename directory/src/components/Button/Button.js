12e822b7e2f2be45dbf3e858604514115416924a
Ben Miller <benbmiller123@gmail.com>
2019-10-31 09:56:43 -0400

    created storyboard, added Button component, added success, danger, and warning varients

---
+++ directory/src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Button.css"
+
+function Button(props) {
+    let classList = "";
+    let types = ["primary","danger","warning","success"];
+    if(types.includes(props.type)) {
+        classList += ` button-${props.type}`
+    }
+    if(props.large) {
+        classList += ` button-large`
+    }
+    return (
+        <input className={classList} value={props.label} type="submit"/>
+    );
+}
+
+export default Button;
