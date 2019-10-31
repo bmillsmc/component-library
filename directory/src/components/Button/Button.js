5988be95ab444b2a7622b3c13fd328a980b413da
Ben Miller <benbmiller123@gmail.com>
2019-10-31 14:47:52 -0400

    added blur and white variants to all colors

--- directory/src/components/Button/Button.js
+++ directory/src/components/Button/Button.js
@@ -3,15 +3,16 @@ import "./Button.css"
 
 function Button(props) {
     let classList = "";
-    let variate = ["white"]
+    let variate = ["white", "blur"];
     let types = ["primary","danger","warning","success"];
     if(types.includes(props.type)) {
         classList += ` button-${props.type}`
+        
     } else {
         classList += ` button-default`;
     }
     if(variate.includes(props.variate)) {
-        classList += ` button-${props.variate}`;
+        classList += `-${props.variate}`;
     }
     return (
         <input className={classList} value={props.label} type="submit"/>
--- directory/src/components/Button/Button.stories.js
+++ directory/src/components/Button/Button.stories.js
@@ -6,25 +6,69 @@ storiesOf("Button", module)
     .add("Default", () => <Button 
         label="Default"
     />)
+    .add("Default White", () => <Button 
+        label="Default"
+        variate="white"
+    />)
+    .add("Default Blur", () => <Button 
+        label="Default"
+        variate="blur"
+    />)
     .add("Primary", () => <Button 
         label="Primary"
         type="primary"
     />)
-    
+    .add("Primary White", () => <Button 
+        label="Primary"
+        type="primary"
+        variate="white"
+    />)
+    .add("Primary Blur", () => <Button 
+        label="Primary"
+        type="primary"
+        variate="blur"
+    />)
     .add('Danger', () => <Button 
         label="Danger"
         type="danger"
     />)
+    .add("Danger White", () => <Button 
+        label="Danger"
+        type="danger"
+        variate="white"
+    />)
+    .add("Danger Blur", () => <Button 
+        label="Danger"
+        type="danger"
+        variate="blur"
+    />)
     .add("Success", () => <Button 
         label="Success" 
         type="success"
-        hover
+    />)
+    .add("Success White", () => <Button 
+        label="Success"
+        type="success"
+        variate="white"
+    />)
+    .add("Success Blur", () => <Button 
+        label="Success"
+        type="success"
+        variate="blur"
     />)
     .add("Warning", () => <Button 
         label="Warning"
         type="warning"
     />)
-    .add("Default-White", () => <Button 
-        label="Default"
+    .add("Warning White", () => <Button 
+        label="Warning"
+        type="warning"
         variate="white"
     />)
+    .add("Warning Blur", () => <Button 
+        label="Warning"
+        type="warning"
+        variate="blur"
+    />)
+    
+    
