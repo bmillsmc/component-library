ae4a396f3a0a6bec9f7e05187a58081fb7834f5b
Ben Miller <benbmiller123@gmail.com>
2019-11-01 10:40:59 -0400

    built SelectInput boilerplate

---
+++ src/components/SelectInput/SelectInput.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./SelectInput.css";
+
+class SelectInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <form>
+
+            </form>
+        );
+    }
+}
+
+export default SelectInput;
---
+++ src/components/SelectInput/SelectInput.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import SelectInput from "./SelectInput";
+
+storiesOf("Select Input", module)
+    .add("Select Input Small", () => <SelectInput 
+
+    />)
