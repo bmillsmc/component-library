20ae62c103db778109c831100874969f8ef1e8d6
Ben Miller <benbmiller123@gmail.com>
2019-11-01 11:00:39 -0400

    created logic for building selectinput

--- src/components/SelectInput/SelectInput.js
+++ src/components/SelectInput/SelectInput.js
@@ -4,19 +4,28 @@ import "./SelectInput.css";
 class SelectInput extends Component {
     constructor(props) {
         super(props);
-
+        let optionList = props.options.slice(0);
+        optionList.unshift(props.fieldName)
         this.state = {
-
+            optionList: optionList
         }
     }
 
     render() {
         return (
-            <form>
-
-            </form>
+            <select>
+                {this.state.optionList.map((element, index) => {
+                    if(index === 0) {
+                        return <option disabled="disabled" selected="selected" value={element} key={index}>{element}</option>
+                    } else {
+                        return <option value={element} key={index}>{element}</option>
+                    }
+                })}
+            </select>
         );
     }
 }
+
+
 
 export default SelectInput;
--- src/components/SelectInput/SelectInput.stories.js
+++ src/components/SelectInput/SelectInput.stories.js
@@ -4,5 +4,6 @@ import SelectInput from "./SelectInput";
 
 storiesOf("Select Input", module)
     .add("Select Input Small", () => <SelectInput 
-
+        fieldName="Select"
+        options={["Option A", "Option B", "Option C"]}
     />)
