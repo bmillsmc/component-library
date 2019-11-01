c2072ca524391569b2514780e0669d1657f996fe
Ben Miller <benbmiller123@gmail.com>
2019-11-01 12:15:26 -0400

    added NumberInput boilerplate

---
+++ src/components/NumberInput/NumberInput.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./NumberInput.css";
+
+class NumberInput extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <form>
+
+            </form>
+        )
+    }
+}
+
+export default NumberInput;
---
+++ src/components/NumberInput/NumberInput.stories.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { storiesOf } from "@storybook/react";
+import NumberInput from "./NumberInput";
+
+storiesOf("Number Input", module)
+    .add("Number Input", () => <NumberInput 
+    
+    />)
